e065dd3351f9410da005a77d4f640dafab3f3903
clawyq <clintonlaw96@gmail.com>
2020-11-09 17:26:39 +0800

    3 pythonise longestsubstr

---
+++ 0003_LongestSubstringWithoutRepeatingCharacters/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+  def lengthOfLongestSubstring(self, s: str) -> int:
+    tokenised: List[str] = list(s)
+    seen: Dict[str, int] = {}
+      
+    leftWin: int = 0; rightWin: int = 0; longest:int = 0
+    for rightWin, char in enumerate(tokenised):
+      if char in seen:
+        leftWin = max(leftWin, seen[char] + 1)
+      
+      seen[char] = rightWin
+      longest = max(longest, rightWin - leftWin + 1)
+    return longest
+        
